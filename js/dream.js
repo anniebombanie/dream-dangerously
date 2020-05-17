e5479fe7457163cfab66804bb4d31d3214b6f051
Annie Liew <anniebombanie@gmail.com>
2020-05-17 01:29:44 -0400

    cleaned up stars JS & CSS

--- js/dream.js
+++ js/dream.js
@@ -1,42 +1,41 @@
 const
-    starsPerHundredPixelsSquare = 1,
-    documentElement = document.documentElement,
-    documentHeight = documentElement.offsetHeight,
-    documentWidth = documentElement.offsetWidth
+	starsPerHundredPixelsSquare = 1,
+	documentElement = document.documentElement,
+	documentHeight = documentElement.offsetHeight,
+	documentWidth = documentElement.offsetWidth
 ;
 
 let
-    starsCount = (
-        (Math.floor(documentHeight / 100) * Math.floor(documentWidth / 100))
-        * starsPerHundredPixelsSquare
-    ),
-    delay = Math.round(starsCount / 5000)
-;
+	starsCount = (
+		(Math.floor(documentHeight / 100) * Math.floor(documentWidth / 100))
+		* starsPerHundredPixelsSquare
+	),
+	delay = Math.round(starsCount / 5000);
 
-if (delay < 1) {
-    delay = 1;
-}
+	if (delay < 1) {
+			delay = 1;
+	}
 
-const intervalId = window.setInterval(
-    () => {
-        const star = document.createElement('i');
+const intervalId = window.setInterval(() => {
+	const star = document.createElement('i');
 
-        star.classList.add('star');
-        star.classList.add(
-            Math.floor((Math.random() * 10) % 2) === 0
-                ? 'small'
-                : 'medium'
-        );
+	star.classList.add('star');
+	star.classList.add(
+		Math.floor((Math.random() * 10) % 2) === 0
+			? 'small'
+			: 'medium'
+		);
 
-        star.style.left = (Math.random() * 99).toFixed(2) + '%';
-        star.style.top = (Math.random() * 99).toFixed(2) + '%';
+		star.style.left = (Math.random() * 99).toFixed(2) + '%';
+		star.style.top = (Math.random() * 99).toFixed(2) + '%';
 
-        document.body.appendChild(star);
+		document.body.appendChild(star);
 
-        if (--starsCount === 0) {
-            window.clearInterval(intervalId);
-        }
-    },
-    delay
+		if (--starsCount === 0) {
+			window.clearInterval(intervalId);
+		}
+	},
+	delay
 );
-// Original JS credit to: https://codepen.io/cliffgurney/pen/GNpzdx
+
+// Star bg credit: https://codepen.io/cliffgurney/pen/GNpzdx
